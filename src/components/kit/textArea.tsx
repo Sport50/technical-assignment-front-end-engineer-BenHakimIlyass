360aef8365dc397c1ec665ad5dc7654685e91dee
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2023-09-20 12:32:43 +0200

    feat(global): Add kit folder that holds the reusable kit components

---
+++ src/components/kit/textArea.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import {
+  TextareaAutosize as BaseTextareaAutosize,
+  TextareaAutosizeProps,
+} from "@mui/base/TextareaAutosize";
+import { styled } from "@mui/system";
+
+export default function TextArea(props: TextareaAutosizeProps) {
+  return <TextareaAutosize {...props} />;
+}
+
+const TextareaAutosize = styled(BaseTextareaAutosize)(
+  () => `
+width: 320px;
+font-family: IBM Plex Sans, sans-serif;
+font-size: 0.875rem;
+font-weight: 400;
+line-height: 1.5;
+padding: 12px;
+border-radius: 12px;
+color: #24292f;
+background: #fff;
+border: 1px solid #d0d7de;
+box-shadow: 0px 2px 24px #daecff;
+
+&:hover {
+  border-color: #3399ff;
+}
+
+&:focus {
+  border-color: #3399ff;
+  box-shadow: 0 0 0 3px #b6daff;
+}
+
+// firefox
+&:focus-visible {
+  outline: 0;
+}
+`
+);
