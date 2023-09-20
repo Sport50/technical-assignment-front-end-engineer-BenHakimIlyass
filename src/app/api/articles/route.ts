34041069e45985efaf7eb3ccace66ccdd57090ce
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2023-09-20 12:47:07 +0200

    fix(article creation): Fix article creation response

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,20 +1,33 @@
 import { NextResponse } from "next/server";
-import articles from "../data";
-
-import fs from "fs";
+import articlesData from "../data";
 
 type GetArticlesResponse = Article[];
-type CreateArticleResponse = Article;
+type CreateArticleResponse = Article[];
+
+let articles = [...articlesData];
 
 export function GET() {
+  console.log(articles);
   return NextResponse.json<GetArticlesResponse>(articles);
 }
 // check this https://dev.to/this-is-learning/readwrite-on-local-json-file-with-nextjs-part-51-8gg
-export function POST(req: Request) {
-  const article = (req.body as unknown) as Article;
+export async function POST(req: Request) {
+  const article = await req.json();
 
-  articles.push(article);
-  return NextResponse.json<CreateArticleResponse>(article);
+  const newArticles = [
+    {
+      ...article,
+      publicationDate: new Date().toString(),
+      author: {
+        email: "email@clubee.com",
+        name: "John",
+      },
+    },
+    ...articles,
+  ];
+
+  articles = newArticles;
+  return NextResponse.json<CreateArticleResponse>(newArticles);
 }
 
 export const dynamic = "force-dynamic";
